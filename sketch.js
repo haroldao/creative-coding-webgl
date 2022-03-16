ec30fc54ef60995d6321af75672105984eb291dc
Harold AO <36776278+haroldao@users.noreply.github.com>
2022-03-16 13:57:12 +0100

    first commit

---
+++ sketch.js
@@ -0,0 +1,40 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 2048, 2048 ]
+};
+
+const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 5;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([ u, v ]);
+      }
+    }
+    return points;
+  }
+
+  const points = createGrid();
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'blue';
+    context.fillRect(0, 0, width, height);
+
+    points.forEach(([ u, v ]) => {
+      const x = u * width;
+      const y = v * height;
+
+      context.beginPath();
+      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.strokeStyle = 'white';
+      context.lineWidth = 20;
+      context.stroke();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
